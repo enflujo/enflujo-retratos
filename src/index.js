08c95ef49785fac799828a7212520723f620364f
Juan Camilo González <j.gonzalezj@uniandes.edu.co>
2022-06-09 23:32:29 -0500

    👽️ pruebas

--- src/index.js
+++ src/index.js
@@ -1,128 +1,56 @@
 import './scss/estilos.scss';
-import vertices from './shaders/vertices.glsl';
-import fragmentos from './shaders/fragmentos.glsl';
-import { createProgramFromSources, resizeCanvasToDisplaySize } from './utilidades/ayudas';
-import nila1 from './imgs/IMG_0495.jpg';
+import urlGato1 from './imgs/gato01.png';
+import urlGato2 from './imgs/gato02.png';
 
-const canvas = document.querySelector('#lienzo');
-const gl = canvas.getContext('webgl2');
-if (!gl) {
-  console.error('Este explorador no es compatible con WebGL2');
+const imgs = [{ url: urlGato1 }, { url: urlGato2 }];
+
+const lienzo = document.getElementById('lienzo');
+const lienzo2 = document.createElement('canvas');
+const ctx = lienzo.getContext('2d');
+const ctx2 = lienzo2.getContext('2d');
+
+lienzo.width = lienzo2.width = 512;
+lienzo.height = lienzo2.height = 512;
+
+Object.assign(lienzo.style, {
+  width: '512px',
+  height: '512px',
+});
+
+Object.assign(lienzo2.style, {
+  width: '512px',
+  height: '512px',
+});
+
+function cargarImgs() {
+  let contador = 0;
+  const total = imgs.length;
+
+  imgs.forEach((instanciaImagen) => {
+    const img = new Image();
+    img.onload = () => {
+      instanciaImagen.img = img;
+      contador++;
+
+      if (contador === total) {
+        inicio();
+      }
+    };
+    img.src = instanciaImagen.url;
+  });
 }
-
-const img1 = new Image();
-img1.onload = inicio;
-img1.src = nila1;
 
 function inicio() {
-  console.log(img1);
-  // Use our boilerplate utils to compile the shaders and link into a program
-  var program = createProgramFromSources(gl, [vertices, fragmentos]);
+  console.log(imgs, imgs[0].img.naturalWidth);
 
-  // look up where the vertex data needs to go.
-  var positionAttributeLocation = gl.getAttribLocation(program, 'a_position');
-  var texCoordAttributeLocation = gl.getAttribLocation(program, 'a_texCoord');
+  ctx2.drawImage(imgs[0].img, 0, 0);
+  const datosImg1 = ctx2.getImageData(0, 0, lienzo2.width, lienzo2.height);
+  ctx2.drawImage(imgs[1].img, 0, 0);
+  const datosImg2 = ctx2.getImageData(0, 0, lienzo2.width, lienzo2.height);
+  const pixeles1 = datosImg1.data;
+  const pixeles2 = datosImg2.data;
 
-  // look up uniform locations
-  var resolutionUniformLocation = gl.getUniformLocation(program, 'u_resolution');
-  var imageLocation = gl.getUniformLocation(program, 'u_image');
-
-  var vao = gl.createVertexArray();
-
-  // and make it the one we're currently working with
-  gl.bindVertexArray(vao);
-
-  // Turn on the attribute
-  var positionBuffer = gl.createBuffer();
-  gl.enableVertexAttribArray(positionAttributeLocation);
-  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-
-  // Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
-  var size = 2; // 2 components per iteration
-  var type = gl.FLOAT; // the data is 32bit floats
-  var normalize = false; // don't normalize the data
-  var stride = 0; // 0 = move forward size * sizeof(type) each iteration to get the next position
-  var offset = 0; // start at the beginning of the buffer
-  gl.vertexAttribPointer(positionAttributeLocation, size, type, normalize, stride, offset);
-
-  var texCoordBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, texCoordBuffer);
-  gl.bufferData(
-    gl.ARRAY_BUFFER,
-    new Float32Array([0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0, 1.0]),
-    gl.STATIC_DRAW
-  );
-  gl.enableVertexAttribArray(texCoordAttributeLocation);
-
-  // Tell the attribute how to get data out of texCoordBuffer (ARRAY_BUFFER)
-  var size = 2; // 2 components per iteration
-  var type = gl.FLOAT; // the data is 32bit floats
-  var normalize = false; // don't normalize the data
-  var stride = 0; // 0 = move forward size * sizeof(type) each iteration to get the next position
-  var offset = 0; // start at the beginning of the buffer
-  gl.vertexAttribPointer(texCoordAttributeLocation, size, type, normalize, stride, offset);
-
-  var texture = gl.createTexture();
-  // make unit 0 the active texture uint
-  // (ie, the unit all other texture commands will affect
-  gl.activeTexture(gl.TEXTURE0 + 0);
-
-  // Bind it to texture unit 0' 2D bind point
-  gl.bindTexture(gl.TEXTURE_2D, texture);
-
-  // Set the parameters so we don't need mips and so we're not filtering
-  // and we don't repeat at the edges
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-
-  // Upload the image into the texture.
-  var mipLevel = 0; // the largest mip
-  var internalFormat = gl.RGBA; // format we want in the texture
-  var srcFormat = gl.RGBA; // format of data we are supplying
-  var srcType = gl.UNSIGNED_BYTE; // type of data we are supplying
-  gl.texImage2D(gl.TEXTURE_2D, mipLevel, internalFormat, srcFormat, srcType, img1);
-
-  resizeCanvasToDisplaySize(gl.canvas, window.devicePixelRatio);
-
-  // Tell WebGL how to convert from clip space to pixels
-  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-
-  // Clear the canvas
-  gl.clearColor(0, 0, 0, 0);
-  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
-  // Tell it to use our program (pair of shaders)
-  gl.useProgram(program);
-
-  // Bind the attribute/buffer set we want.
-  gl.bindVertexArray(vao);
-
-  // Pass in the canvas resolution so we can convert from
-  // pixels to clipspace in the shader
-  gl.uniform2f(resolutionUniformLocation, gl.canvas.width, gl.canvas.height);
-
-  // Tell the shader to get the texture from texture unit 0
-  gl.uniform1i(imageLocation, 0);
-
-  // Bind the position buffer so gl.bufferData that will be called
-  // in setRectangle puts data in the position buffer
-  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-
-  setRectangle(gl, 0, 0, img1.naturalWidth, img1.naturalHeight);
-
-  // Draw the rectangle.
-  var primitiveType = gl.TRIANGLES;
-  var offset = 0;
-  var count = 6;
-  gl.drawArrays(primitiveType, offset, count);
+  // ctx.
 }
 
-function setRectangle(gl, x, y, width, height) {
-  var x1 = x;
-  var x2 = x + width;
-  var y1 = y;
-  var y2 = y + height;
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([x1, y1, x2, y1, x1, y2, x1, y2, x2, y1, x2, y2]), gl.STATIC_DRAW);
-}
+cargarImgs();
